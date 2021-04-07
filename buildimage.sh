31df10700b06ac466ab5b9a987ecc34627a90da6
tsubiya <37928861+tsubiya@users.noreply.github.com>
2021-04-08 02:13:47 +0530

    Update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -1,4 +1,4 @@
-
+# push image to ecr
 version=$BUILD_ID
 docker build -t apprepo:$version .
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 040168646982.dkr.ecr.us-east-2.amazonaws.com
@@ -9,4 +9,9 @@ docker push 040168646982.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
 
 echo "Image Successfully pushed to ecr"
 
+#create new taskdefinition with new version
 
+sed -i s#040168646982.dkr.ecr.us-east-2.amazonaws.com/apprepo:9#040168646982.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version#g taskdefination.json
+
+#register task defination
+aws ecs register-task-definition  --cli-input-json  file://taskdefination.json --region us-east-2
